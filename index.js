8bdb8212fe8e7d4daa7152c233dc2a754e865051
github username <github email address>
2021-04-05 16:12:15 -0500

    commit tree 66c168faf6db979a7ea39bb8f55cc6309bd4dbc5

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+    return drivers
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+    return drivers
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop()
+    return drivers
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift()
+    return drivers
+}
+
+function appendDriver(name) {
+    const newArr = [...drivers, name]
+    return newArr
+}
+
+function prependDriver(name) {
+    const newArr = [name, ...drivers]
+    return newArr
+}
+
+function removeLastDriver() {
+    const newArr = drivers.slice(0, (drivers.length-1))
+    return newArr
+}
+
+function removeFirstDriver() {
+    const newArr = drivers.slice(1)
+    return newArr
+}
